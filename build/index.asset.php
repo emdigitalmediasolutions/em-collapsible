7cc41a4376d21b54c960fb6f13a80380c3195727
NetworkMonk <jamesplant@gmail.com>
2020-06-28 11:14:46 +0100

    Import over from plugin template

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-polyfill'), 'version' => 'de274af573035f45a1b2c73362e9a6fb');
---
+++ em-collapse.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Plugin Name:     EM Collapsible
+ * Description:     A simple collapsible element that allows you to enter a title and description, the description is hidden until the user expands it.
+ * Version:         1.0.0
+ * Author:          The WordPress Contributors
+ * License:         GPL-2.0-or-later
+ * License URI:     https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:     create-block
+ *
+ * @package         create-block
+ */
+
+
+/**
+ * Registers all block assets so that they can be enqueued through the block editor
+ * in the corresponding context.
+ *
+ * @see https://developer.wordpress.org/block-editor/tutorials/block-tutorial/applying-styles-with-stylesheets/
+ */
+function create_block_em_collapsible_block_init() {
+	$dir = dirname( __FILE__ );
+
+	$script_asset_path = "$dir/build/index.asset.php";
+	if ( ! file_exists( $script_asset_path ) ) {
+		throw new Error(
+			'You need to run `npm start` or `npm run build` for the "create-block/em-collapsible" block first.'
+		);
+	}
+	$index_js     = 'build/index.js';
+	$script_asset = require( $script_asset_path );
+	wp_register_script(
+		'create-block-em-collapsible-block-editor',
+		plugins_url( $index_js, __FILE__ ),
+		$script_asset['dependencies'],
+		$script_asset['version']
+	);
+
+	$editor_css = 'build/index.css';
+	wp_register_style(
+		'create-block-em-collapsible-block-editor',
+		plugins_url( $editor_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$editor_css" )
+	);
+
+	$style_css = 'build/style-index.css';
+	wp_register_style(
+		'create-block-em-collapsible-block',
+		plugins_url( $style_css, __FILE__ ),
+		array(),
+		filemtime( "$dir/$style_css" )
+	);
+
+	register_block_type( 'create-block/em-collapsible', array(
+		'editor_script' => 'create-block-em-collapsible-block-editor',
+		'editor_style'  => 'create-block-em-collapsible-block-editor',
+		'style'         => 'create-block-em-collapsible-block',
+	) );
+
+}
+add_action( 'init', 'create_block_em_collapsible_block_init' );
+
